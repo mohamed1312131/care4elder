97e439238f36d0b0e7a8981496306dbf00076574
mohamed1312131 <mohamed1312131@gmail.com>
2025-04-06 21:34:12 +0100

    first

---
+++ user-service/src/main/java/com/care4elders/userservice/controller/UserController.java
@@ -0,0 +1,43 @@
+package com.care4elders.userservice.controller;
+
+import com.care4elders.userservice.UserService;
+import com.care4elders.userservice.entity.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
+    }
+}
